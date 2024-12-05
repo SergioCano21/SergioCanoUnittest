abf75c2f59fdc6ae569c4db9a1cde3f6e64a7c79
SergioCano21 <chimo.c.g@hotmail.com>
2024-12-04 19:54:00 -0600

    config fix

--- src/test/java/com/mayab/quality/functional/CRUDSeleniumTest.java
+++ src/test/java/com/mayab/quality/functional/CRUDSeleniumTest.java
@@ -6,6 +6,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.fail;
 
 import org.junit.jupiter.api.Test;
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
@@ -13,9 +14,13 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
@@ -27,6 +32,7 @@ public class CRUDSeleniumTest {
 	private String baseUrl;
 	private StringBuffer verificationErrors = new StringBuffer();
 	JavascriptExecutor js;
+	ChromeOptions options;
   
 	private void pause(long milis) {
 		try {
@@ -40,97 +46,94 @@ public class CRUDSeleniumTest {
 	@BeforeEach
 	public void setUp() throws Exception {
 		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
+		options = new ChromeOptions();
+		options.addArguments("--headless");
+		options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1400,800");
+		driver = new ChromeDriver(options);
 		baseUrl = "https://mern-crud-mpfr.onrender.com/";
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
 		js = (JavascriptExecutor) driver;
 	}
+	
+	public void takeScreenshot(String fileName) throws IOException {
+    	File file = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+    	FileUtils.copyFile(file, new FileOutputStream("src/screenshots/" + fileName + ".jpeg"));
+    }
   
 	@Test
 	@Order(1)
 	public void Add_User_Correct() throws Exception {
 		driver.get(baseUrl);
-		pause(1000);
 		driver.findElement(By.xpath("//div[@id='root']/div/div[2]/button")).click();
 	    driver.findElement(By.name("name")).click();
 	    driver.findElement(By.name("name")).clear();
 	    driver.findElement(By.name("name")).sendKeys("Sergio Cano");
-	    pause(1000);
 	    driver.findElement(By.name("email")).click();
 	    driver.findElement(By.name("email")).clear();
 	    driver.findElement(By.name("email")).sendKeys("sergio.cano@gmail.com");
-	    pause(1000);
 	    driver.findElement(By.name("age")).click();
 	    driver.findElement(By.name("age")).clear();
 	    driver.findElement(By.name("age")).sendKeys("22");
-	    pause(1000);
 	    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Gender'])[2]/following::div[2]")).click();
 	    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Male'])[1]/following::div[2]")).click();
-	    pause(1000);
 	    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Woah!'])[1]/following::button[1]")).click();
 	    pause(1000);
+	    takeScreenshot("Add_User_Correct");
 	    String result = driver.findElement(By.xpath("/html/body/div[3]/div/div[2]/form/div[4]/div/p")).getText();
 	    assertThat(result, is("Successfully added!"));
-	    driver.findElement(By.xpath("//i")).click();
-	    pause(1000);
+	    driver.findElement(By.xpath("//i")).click();	
 	}
 	
 	@Test
 	@Order(2)
 	public void Add_User__Not_Correct() throws Exception {
 		driver.get(baseUrl);
-		pause(1000);
 		driver.findElement(By.xpath("//div[@id='root']/div/div[2]/button")).click();
 	    driver.findElement(By.name("name")).click();
 	    driver.findElement(By.name("name")).clear();
 	    driver.findElement(By.name("name")).sendKeys("Sergio Cano");
-	    pause(1000);
 	    driver.findElement(By.name("email")).click();
 	    driver.findElement(By.name("email")).clear();
 	    driver.findElement(By.name("email")).sendKeys("sergio.cano@gmail.com");
-	    pause(1000);
 	    driver.findElement(By.name("age")).click();
 	    driver.findElement(By.name("age")).clear();
 	    driver.findElement(By.name("age")).sendKeys("22");
-	    pause(1000);
 	    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Gender'])[2]/following::div[2]")).click();
 	    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Male'])[1]/following::div[2]")).click();
-	    pause(1000);
 	    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Woah!'])[1]/following::button[1]")).click();
 	    pause(1000);
-	    
+	    takeScreenshot("Add_User__Not_Correct");
 	    String result = driver.findElement(By.xpath("/html/body/div[3]/div/div[2]/form/div[5]/div/p")).getText();
 	    assertThat(result, is("That email is already taken."));
 	    driver.findElement(By.xpath("//i")).click();
-	    pause(1000);
 	}
 
 	@Test
 	@Order(3)
-	public void Update_User_Age() {
+	public void Update_User_Age() throws Exception {
 		driver.get(baseUrl);
 	    driver.findElement(By.xpath("//div[@id='root']/div/div[2]/table/tbody/tr/td[5]/button")).click();
-	    driver.findElement(By.name("age")).click();
 	    pause(1000);
+	    driver.findElement(By.name("age")).click();
 	    driver.findElement(By.name("age")).clear();
 	    driver.findElement(By.name("age")).sendKeys("30");
-	    pause(1000);
 	    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Woah!'])[1]/following::button[1]")).click();
 	    pause(1000);
+	    takeScreenshot("Update_User_Age");
 	    String result = driver.findElement(By.xpath("/html/body/div[3]/div/div[2]/form/div[4]/div/p")).getText();
 	    assertThat(result, is("Successfully updated!"));
 	    driver.findElement(By.xpath("//i")).click();
-	    pause(1000);
 	}
 	
 	@Test
 	@Order(6)
-	public void Delete_User() {
+	public void Delete_User() throws Exception {
 		driver.get(baseUrl);
 		driver.findElement(By.xpath("//div[@id='root']/div/div[2]/table/tbody/tr/td[5]/button[2]")).click();
 		pause(1000);
+		takeScreenshot("Delete_User");
 	    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Sergio Cano'])[2]/following::button[1]")).click();
-	    pause(1000);
 	}
 	
 	@Test
@@ -180,17 +183,6 @@ public class CRUDSeleniumTest {
             }
             tableData.add(rowData);
         }
-        
-        /*boolean result = false;
-        List<String> user_data = new ArrayList<>();
-        for (List<String> list: tableData) {
-        	if (list.contains("Sergio Cano")) {
-                result = true;
-                user_data = list;
-                break;
-            }
-        }*/
-        //assertThat(result, is(true));
         
         System.out.println("------------------------");
         System.out.println(tableData);
